9cc8f81c6730c79e8766d44bd702f81e85ee21a3
Patrick McGleenon <pmcgleenon@users.noreply.github.com>
2025-11-04 18:56:54 +0000

    datafusion-50 updates

--- datafusion/benchmark.sh
+++ datafusion/benchmark.sh
@@ -11,12 +11,19 @@ sudo apt-get update -y
 sudo apt-get install -y gcc
 
 echo "Install DataFusion main branch"
-git clone https://github.com/apache/arrow-datafusion.git
-cd arrow-datafusion/
-git checkout 47.0.0
-CARGO_PROFILE_RELEASE_LTO=true RUSTFLAGS="-C codegen-units=1" cargo build --release --package datafusion-cli --bin datafusion-cli
-export PATH="`pwd`/target/release:$PATH"
-cd ..
+if [ -f "/usr/local/bin/datafusion-cli" ]; then
+    echo "datafusion-cli already exists in /usr/local/bin, skipping compilation"
+    export PATH="/usr/local/bin:$PATH"
+    echo "Current version: $(/usr/local/bin/datafusion-cli --version)"
+else
+    git clone https://github.com/apache/arrow-datafusion.git
+    cd arrow-datafusion/
+    git checkout 50.0.0
+    CARGO_PROFILE_RELEASE_LTO=true RUSTFLAGS="-C codegen-units=1" cargo build --release --package datafusion-cli --bin datafusion-cli
+    sudo cp target/release/datafusion-cli /usr/local/bin/
+    export PATH="/usr/local/bin:$PATH"
+    cd ..
+fi
 
 echo "Download benchmark target data, single file"
 wget --continue --progress=dot:giga https://datasets.clickhouse.com/hits_compatible/hits.parquet
